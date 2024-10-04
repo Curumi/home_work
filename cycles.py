0c027c451c5b45cbe7bb751b67bba979ad198d5d
Curumi <Aspirel674@gmail.com>
2024-10-04 20:12:34 +0300

    homework
    Linux is evil

---
+++ cycles.py
@@ -0,0 +1,62 @@
+count = 0
+
+while count < 6:
+
+        print(count)
+
+        count += 2
+
+
+
+
+
+collection = [1,2,3,4,5,6,7,8,9,10]
+
+n = 10
+
+for i in collection:
+
+      l = n*i
+
+      print(l)
+
+
+
+
+n = 1
+
+while n < 6:
+
+    print('Цикл выполнился', n, 'раз(а)')
+
+n = n+1
+
+
+
+
+
+
+
+for i in range(10, 5, -2):
+      print(i)
+
+
+
+
+
+
+
+n = 6
+
+while n > 0:
+
+        n -= 1
+
+        print(n)
+
+
+
+
+
+for i in range(1, 33):
+    print(f'буква номер {i}')
