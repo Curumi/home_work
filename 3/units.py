bcc6bf6ff502ff85044c2b396a50a7574b932ab8
Curumi <aspirel674@gmail.com>
2025-02-01 15:48:38 +0300

    add

--- 3/units.py
+++ 3/units.py
@@ -1,14 +1,15 @@
 import world
-import Missile_collections
+import  texture as skin
 from hitbox import Hitbox
-import texture as skin
 from tkinter import NW
 from random import randint
 
+import missiles_collection
 
 class Unit:
-    def __init__(self, canvas, x, y, speed, padding, bot, default_image):
-        self.destroyed = False
+    def __init__(self, canvas, x,y, speed, padding, bot, default_image):
+
+        self._destroyed = False
 
         self._speed = speed
         self._x = x
@@ -20,16 +21,21 @@ class Unit:
         self._dx = 0
         self._dy = 0
         self._bot = bot
-        self._bot = bot
-        self._hitbox = Hitbox(x, y, world.BLOCK_SIZE, world.BLOCK_SIZE, padding=padding)
+        self._hitbox = Hitbox(x,
+                              y,
+                              world.BLOCK_SIZE,
+                              world.BLOCK_SIZE,
+                              padding=padding)
+
         self._default_image = default_image
-        self._forward_image = default_image
-        self._backward_image = default_image
         self._left_image = default_image
         self._right_image = default_image
+        self._forward_image = default_image
+        self._backward_image = default_image
         self._create()
 
-    def damage(self,value):
+# 12_4
+    def damage(self, value):
         self._hp -= value
         if self._hp <= 0:
             self.destroy()
@@ -38,15 +44,19 @@ class Unit:
 
 
     def is_destroyed(self):
-        return self.destroyed
+        return self._destroyed
 
     def destroy(self):
-        self.destroyed = True
+        self._destroyed = True
         self.stop()
         self._speed = 0
 
+
+
+
     def _create(self):
-        self._id = self._canvas.create_image(self._x, self._y,
+        self._id = self._canvas.create_image(self._x,
+                                             self._y,
                                              image=skin.get(self._default_image),
                                              anchor=NW)
 
@@ -55,31 +65,22 @@ class Unit:
             self._canvas.delete(self._id)
         except Exception:
             pass
-
-    def forward(self):
+    def forvard(self):
         self._vx = 0
         self._vy = -1
-        self._canvas.itemconfig(self._id,
-                                image=skin.get(self._forward_image))
-
+        self._canvas.itemconfig(self._id, image=skin.get(self._forward_image))
     def backward(self):
         self._vx = 0
         self._vy = 1
-        self._canvas.itemconfig(self._id,
-                                image=skin.get(self._backward_image))
-
+        self._canvas.itemconfig(self._id, image=skin.get(self._backward_image))
     def left(self):
         self._vx = -1
         self._vy = 0
-        self._canvas.itemconfig(self._id,
-                                image=skin.get(self._left_image))
-
+        self._canvas.itemconfig(self._id, image=skin.get(self._left_image))
     def right(self):
         self._vx = 1
         self._vy = 0
-        self._canvas.itemconfig(self._id,
-                                image=skin.get(self._right_image))
-
+        self._canvas.itemconfig(self._id, image=skin.get(self._right_image))
     def stop(self):
         self._vx = 0
         self._vy = 0
@@ -91,14 +92,11 @@ class Unit:
         self._dy = self._vy * self._speed
         self._x += self._dx
         self._y += self._dy
+
         self._update_hitbox()
         self._check_map_collision()
         self._repaint()
 
-    def _repaint(self):
-        screen_x = world.get_screen_x(self._x)
-        screen_y = world.get_screen_y(self._y)
-        self._canvas.moveto(self._id, x=screen_x, y=screen_y)
 
     def _AI(self):
         pass
@@ -120,6 +118,10 @@ class Unit:
     def _on_map_collision(self, details):
         pass
 
+    def _repaint(self):
+        screen_x = world.get_screen_x(self._x)
+        screen_y = world.get_screen_y(self._y)
+        self._canvas.moveto(self._id, x=screen_x, y=screen_y)
     def _undo_move(self):
         if self._dx == 0 and self._dy == 0:
             return
@@ -130,24 +132,23 @@ class Unit:
         self._dx = 0
         self._dy = 0
 
-    def intersect(self, other_unit):
+    def intersects(self, other_unit):
         value = self._hitbox.intersects(other_unit._hitbox)
         if value:
-            self._on_intersect(other_unit)
+            self._on_intersects(other_unit)
         return value
-
-    def _on_intersect(self, other_unit):
+    def _on_intersects(self, other_unit):
         self._undo_move()
 
     def _change_orientation(self):
         rand = randint(0, 3)
         if rand == 0:
             self.left()
-        if rand == 1:
-            self.forward()
-        if rand == 2:
+        elif rand == 1:
+            self.forvard()
+        elif rand == 2:
             self.right()
-        if rand == 3:
+        elif rand == 3:
             self.backward()
 
     def get_hp(self):
@@ -177,9 +178,13 @@ class Unit:
 
 class Tank(Unit):
     def __init__(self, canvas, row, col, bot=True):
-        super().__init__(canvas, col * world.BLOCK_SIZE, row * world.BLOCK_SIZE, 2, 8,
-                         bot, 'tank_up')
-
+        super().__init__(canvas,
+                         col*world.BLOCK_SIZE,
+                         row*world.BLOCK_SIZE,
+                         2,
+                         8,
+                         bot,
+                         'tank_up' )
         if bot:
             self._forward_image = 'tank_up'
             self._backward_image = 'tank_down'
@@ -191,39 +196,63 @@ class Tank(Unit):
             self._left_image = 'tank_left_player'
             self._right_image = 'tank_right_player'
 
-        self.forward()
+        # if bot:
+        #     self._left_image = 'tankT34_left'
+        #     self._right_image = 'tankT34_right'
+        #     self._forward_image = 'tankT34_forward'
+        #     self._backward_image = 'tankT34_backward'
+        # else:
+        #     self._left_image = 'tank_left_player'
+        #     self._right_image = 'tank_right_player'
+        #     self._forward_image = 'tank_forward_player'
+        #     self._backward_image = 'tank_backward_player'
+
+        self.forvard()
         self._ammo = 80
         self._usual_speed = self._speed
-        self._water_speed = self._speed // 2
+        self._water_speed = self._speed//2
         self._target = None
 
     def set_target(self, target):
         self._target = target
 
+
     def _AI_goto_target(self):
-        if randint(1, 2) == 1:
+        if randint(1,2) == 1:
             if self._target.get_x() < self.get_x():
                 self.left()
             else:
                 self.right()
         else:
             if self._target.get_y() < self.get_y():
-                self.forward()
+                self.forvard()
             else:
                 self.backward()
 
-    def get_ammo(self):
-        return self._ammo
+    def _AI(self):
+        if randint(1,30) ==1:
+            if randint(1,10) < 9 and self._target is not None:
+                self._AI_goto_target()
+            else:
+                self._change_orientation()
+        elif randint(1,30) == 1:
+            self._AI_fire()
+        elif randint(1,100) == 1:
+            self.fire()
+# 12_2 Стрельба из танка
+    def fire(self):
+        if self._ammo > 0:
+            self._ammo -= 1
+            missiles_collection.fire(self)
 
     def _take_ammo(self):
         self._ammo += 10
         if self._ammo > 100:
-            self._ammo1 = 100
+            self._ammo = 100
 
-    def fire(self):
-        if self._ammo > 0:
-            self._ammo -= 1
-            Missile_collections.fire(self)
+    def get_ammo(self):
+        return self._ammo
+
 
     def _set_usual_speed(self):
         self._speed = self._usual_speed
@@ -236,42 +265,34 @@ class Tank(Unit):
             self._set_water_speed()
         elif world.MISSLE in details:
             pos = details[world.MISSLE]
-            if world.take(pos['row'], pos['col']) != world.AIR:
+            if world.take(pos['row'], pos['col'])!= world.AIR:
                 self._take_ammo()
         else:
             self._undo_move()
             if self._bot:
                 self._change_orientation()
 
+
     def _no_map_collision(self):
         self._set_usual_speed()
 
-    def _on_intersect(self, other_unit):
-        super()._on_intersect(other_unit)
+    def _on_intersects(self, other_unit):
+        super()._on_intersects(other_unit)
         if self._bot:
             self._change_orientation()
 
-    def _AI(self):
-        if randint(1, 30) == 1:
-            if randint(1, 10) < 9 and self._target is not None:
-                self._AI_goto_target()
-            else:
-                self._change_orientation()
-        elif randint(1, 30) == 1:
-            self._AI_fire()
-        elif randint(1, 100) == 1:
-            self.fire()
-
-
+# 12_6
     def _AI_fire(self):
         if self._target is None:
             return
 
-        center_x = self.get_x() + self.get_size() // 2
-        center_y = self.get_y() + self.get_size() // 2
+        center_x = self.get_x() + self.get_size()//2
+        center_y = self.get_y() + self.get_size()//2
 
-        target_center_x = self._target.get_x() + self._target.get_size() // 2
-        target_center_y = self._target.get_y() + self._target.get_size() // 2
+        target_center_x = (self._target.get_x() +
+                           self._target.get_size()//2)
+        target_center_y = (self._target.get_y() +
+                           self._target.get_size()//2)
 
         row = world.get_row(center_y)
         col = world.get_col(center_x)
@@ -289,37 +310,43 @@ class Tank(Unit):
 
         elif col == col_target:
             if row_target < row:
-                self.forward()
+                self.forvard()
                 self.fire()
             else:
                 self.backward()
                 self.fire()
 
 class Missile(Unit):
-    def __init__(self, canvas,owner):
-        super().__init__(canvas,owner.get_x(), owner.get_y(), 6, 20, False, 'missile_up')
-
+    def __init__(self, canvas, owner):
+        super().__init__(canvas,
+                         owner.get_x(),
+                         owner.get_y(),
+                         6,
+                         20,
+                         False,
+                         'missile_up')
 
         self._forward_image = 'missile_up'
         self._backward_image = 'missile_down'
-        self._right_image = 'missile_right'
         self._left_image = 'missile_left'
+        self._right_image = 'missile_right'
         self._owner = owner
 
-        if owner.get_vx() == 1 and owner.get_vy() ==0:
+        if owner.get_vx() == 1 and owner.get_vy() == 0:
             self.right()
-
-        if owner.get_vx() == -1 and owner.get_vy() == 0:
+        elif owner.get_vx() == -1 and owner.get_vy() == 0:
             self.left()
 
-        if owner.get_vx() == 0 and owner.get_vy() == -1:
-            self.forward()
-
-        if owner.get_vx() == 0 and owner.get_vy() == 1:
+        elif owner.get_vx() == 0 and owner.get_vy() == -1:
+            self.forvard()
+        elif owner.get_vx() == 0 and owner.get_vy() == 1:
             self.backward()
 
-        self._x += owner.get_vx() * self.get_size()//2
-        self._y += owner.get_vy() * self.get_size()//2
+        self._x += owner.get_vx() * self.get_size() // 2
+        self._y += owner.get_vy() * self.get_size() // 2
+
+
+
         self._hitbox.set_blacklist([world.CONCRETE, world.BRICK])
 
     def get_owner(self):
@@ -330,7 +357,27 @@ class Missile(Unit):
             row = details[world.BRICK]['row']
             col = details[world.BRICK]['col']
             world.destroy(row, col)
+
+
+
             self.destroy()
 
         if world.CONCRETE in details:
             self.destroy()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
